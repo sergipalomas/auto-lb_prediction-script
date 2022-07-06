ce5dbb367b7537d99d3bd721049a32a4d9b94732
spalomas <sergi.palomas@bsc.es>
2022-07-06 13:39:20 +0200

    Change CHPSY --> CHSY

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def print_result(num_components, list_components_class_interpolated, optimal_res
         print("Results for component %s:" % component.name)
         print("Number of processes: %i" % opt_nproc)
         print("Fitness: %.2f" % component.get_fitness([opt_nproc]).fitness)
-        print("CHPSY: %i" % (component.get_chsy(opt_nproc)))
+        print("CHSY: %i" % (component.get_chsy(opt_nproc)))
         print("SYPD: %.2f" % component.get_sypd(opt_nproc))
         component.plot_scalability(opt_nproc)
         component.plot_scalability_n(opt_nproc)
@@ -113,13 +113,13 @@ def print_result(num_components, list_components_class_interpolated, optimal_res
         chsy_acc += component.get_chsy(opt_nproc)
 
     if num_components > 1:
-        # We have to add the cpl_cost to the CHPSY
+        # We have to add the cpl_cost to the CHSY
         chsy_acc += optimal_result['cpl_chsy']
         print("\n -------------------------------\n")
         print("Total number of processes: %i" % nproc_acc)
-        print("Expected coupled CHPSY: %i" % chsy_acc)
+        print("Expected coupled CHSY: %i" % chsy_acc)
         print("Expected coupled SYPD: %.2f" % optimal_result['SYPD'])
-        print("Expected coupling cost: %.2f %%, (%.2f CHPSY)" % (optimal_result['cpl_cost']*100, optimal_result['cpl_chsy']))
+        print("Expected coupling cost: %.2f %%, (%.2f CHSY)" % (optimal_result['cpl_cost']*100, optimal_result['cpl_chsy']))
         print("%s/%s speed ratio: %.2f" % (list_components_class_interpolated[0].name, list_components_class_interpolated[1].name, optimal_result['speed_ratio']))
         print("Coupled Objective Function: %.3f" % optimal_result['objective_f'])
 
@@ -210,12 +210,11 @@ if __name__ == "__main__":
         c1_n = Component(c.name, comp_interpolated.nproc, comp_interpolated.SYPD,
                          c.nproc_restriction, ts_info_df, ts_info_nproc, TTS_r, ETS_r)
         list_components_class_interpolated.append(c1_n)
+        c1_n.plot_scalability()
 
     if show_plots:
         check_interpo(num_components, list_components_class_interpolated, list_components_interpolated)
 
     from brute_force import new_brute_force
-    print("Check fitness metric")
     optimal_result = new_brute_force(num_components, list_components_class_interpolated, max_nproc, show_plots)
-
     print_result(num_components, list_components_class_interpolated, optimal_result)

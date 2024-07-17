7e907d30c0a1fd7b360118bc00454518861ee23d
sergipalomas <sergipalomas@gmail.com>
2024-07-17 13:28:39 +0200

    Update to GNU GPL v3 in prediction-script

--- brute_force.py
+++ brute_force.py
@@ -1,5 +1,20 @@
-import time
+# This file is part of Auto LB Workflow.
+#
+# Auto LB Workflow is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Auto LB Workflow is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Auto LB Workflow. If not, see <http://www.gnu.org/licenses/>.
 
+
+import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
